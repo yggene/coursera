f400fdaad4bd6a5649d6729dc882f7ebd7b7833e
yggene <49290157+yggene@users.noreply.github.com>
2020-04-05 13:47:23 +0300

    added 2 solutions to week 1 folder

---
+++ interfaces/1. JS/week1/addNumbers.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Number} a Первое слагаемое
+ * @param {Number} b Второе слагаемое
+ * @returns {Number}
+ */
+module.exports = function (a, b) {
+    var result = Number(a) + Number(b);
+    return result;
+};
