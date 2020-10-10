cb5e81db7f8af387b8533eb33451ba7e5c621f4e
Evgeny Alekseev <yggene@gmail.com>
2020-10-10 23:18:34 +0300

    removed Interfaces folder

--- interfaces/1. JS/week1/addNumbers.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {Number} a Первое слагаемое
- * @param {Number} b Второе слагаемое
- * @returns {Number}
- */
-module.exports = function (a, b) {
-    var result = Number(a) + Number(b);
-    return result;
-};
